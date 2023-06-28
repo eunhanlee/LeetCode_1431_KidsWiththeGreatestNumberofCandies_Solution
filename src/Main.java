d81e98d6f5c49ac01689e7e504556813f41f3a38
eunhanlee58 <eunhanlee58@gmail.com>
2023-06-28 08:47:38 -0400

    add main with testcase

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution tt = new Solution();
+        int[] candies = {2, 3, 5, 1, 3};
+        int extraCandy = 3;
+        System.out.println(tt.kidsWithCandies(candies, extraCandy));
     }
 }
